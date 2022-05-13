264e2064f49af3e3085be61eb6febb72dc1174ce
lllllllllwith10ls <38187754+lllllllllwith10ls@users.noreply.github.com>
2022-05-13 18:17:15 -0500

    Update paperclip.js

--- paperclip.js
+++ paperclip.js
@@ -1,11 +1,19 @@
 elements.paperclip = {
 	color: "#bdbdbd",
-	behavior: behaviors.POWDER,
 	tempHigh: 1538,
 	stateHigh: "molten_iron",
 	category: "powders",
 	state: "solid",
 	density: 7860,
 	conduct: 0.47,
-	hardness: 0.4
+	hardness: 0.4,
+	properties: {
+		link1x: -1,
+		link1y: -1,
+		link2x: -1,
+		link2y: -1
+	},
+	tick: function(pixel) {
+		behaviors.POWDER();
+	}
 }
