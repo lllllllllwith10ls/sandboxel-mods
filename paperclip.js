2ad043fac27cae570987a2e6cfdecd6b8b328e9f
lllllllllwith10ls <38187754+lllllllllwith10ls@users.noreply.github.com>
2022-05-13 17:53:07 -0500

    Add files via upload

---
+++ paperclip.js
@@ -0,0 +1,11 @@
+elements.paperclip = {
+	color: "#bdbdbd",
+	behavior: behaviors.POWDER,
+	tempHigh: 1538,
+	stateHigh: "molten_iron",
+	category: "powders",
+	state: "solid",
+	density: 7860,
+	conduct: 0.47,
+	hardness: 0.4
+}
