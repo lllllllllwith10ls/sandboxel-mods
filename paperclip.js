75873deb34175b23b2e1767a0602d220dd049266
lllllllllwith10ls <38187754+lllllllllwith10ls@users.noreply.github.com>
2022-05-13 21:01:58 -0500

    Update paperclip.js

--- paperclip.js
+++ paperclip.js
@@ -33,15 +33,50 @@ elements.paperclip = {
 		let prevPosX = pixel.x;
 		let prevPosY = pixel.y;
 		behaviors.POWDER(pixel);
-		if(prevPosX === pixel.x && prevPosY === pixel.Y)
-			   return;
-		if (Math.random() < 0.5) {
+		if(!(prevPosX === pixel.x && prevPosY === pixel.Y))
+		{
+			if (Math.random() < 0.5) {
+				var adjpixel = pixel.link1;
+				if (adjpixel !== null && adjpixel.element === "paperclip") {
+					if(!recursiveMove(adjpixel, prevPosX, prevPosY,true,0))
+					{
+						pixel.link1 = null;
+						adjpixel.link1 = null;
+					}
+				} else {
+					pixel.link1 = null;
+				}
+			} else {
+				var adjpixel = pixel.link2;
+				if (adjpixel !== null && adjpixel.element === "paperclip") {
+					if(!recursiveMove(adjpixel, prevPosX, prevPosY,false,0))
+					{
+						pixel.link2 = null;
+						adjpixel.link2 = null;
+					}
+				} else {
+					pixel.link2 = null;
+				}
+			}
+		}			
+	}
+}
+function recursiveMove(pixel,x,y,left,iters) {
+	let prevPosX = pixel.x;
+	let prevPosY = pixel.y;
+	if(iters > 50 || !tryMove(pixel,x,y)) {
+		return false;
+	}
+	if(!(prevPosX === pixel.x && prevPosY === pixel.Y))
+	{	
+		if(!left) {
 			var adjpixel = pixel.link1;
 			if (adjpixel !== null && adjpixel.element === "paperclip") {
-				if(!recursiveMove(adjpixel, prevPosX, prevPosY,true,0))
+				if(!recursiveMove(adjpixel, prevPosX, prevPosY,!left,iters+1))
 				{
 					pixel.link1 = null;
 					adjpixel.link1 = null;
+									
 				}
 			} else {
 				pixel.link1 = null;
@@ -49,7 +84,7 @@ elements.paperclip = {
 		} else {
 			var adjpixel = pixel.link2;
 			if (adjpixel !== null && adjpixel.element === "paperclip") {
-				if(!recursiveMove(adjpixel, prevPosX, prevPosY,false,0))
+				if(!recursiveMove(adjpixel, prevPosX, prevPosY,!left,iters+1))
 				{
 					pixel.link2 = null;
 					adjpixel.link2 = null;
@@ -59,37 +94,5 @@ elements.paperclip = {
 			}
 		} 
 	}
-}
-function recursiveMove(pixel,x,y,left,iters) {
-	let prevPosX = pixel.x;
-	let prevPosY = pixel.y;
-	if(iters > 50 || !tryMove(pixel,x,y)) {
-                return false;
-        }
-
-        if(!left) {
-	        var adjpixel = pixel.link1;
-	        if (adjpixel !== null && adjpixel.element === "paperclip") {
-			if(!recursiveMove(adjpixel, prevPosX, prevPosY,!left,iters+1))
-			{
-				pixel.link1 = null;
-				adjpixel.link1 = null;
-                                
-			}
-		} else {
-			pixel.link1 = null;
-		}
-	} else {
-		var adjpixel = pixel.link2;
-		if (adjpixel !== null && adjpixel.element === "paperclip") {
-			if(!recursiveMove(adjpixel, prevPosX, prevPosY,!left,iters+1))
-			{
-				pixel.link2 = null;
-				adjpixel.link2 = null;
-			}
-		} else {
-			pixel.link2 = null;
-		}
-	} 
-return true;
+	return true;
 }
