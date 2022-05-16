f5fb9861b97b34c61e351b36b4b1a9db3ada4eee
lllllllllwith10ls <38187754+lllllllllwith10ls@users.noreply.github.com>
2022-05-16 12:36:23 -0500

    Update paperclip.js

--- paperclip.js
+++ paperclip.js
@@ -19,7 +19,7 @@ elements.paperclip = {
 			if(!(dx === 0 || dy === 0) && !isEmpty(pixel.x+dx, pixel.y+dy, true))
 			{
 				let adjpixel = pixelMap[pixel.x+dx][pixel.y+dy];
-				if (adjpixel.element === "paperclip") {
+				if (adjpixel.element === "paperclip" || adjpixel.element === "paper") {
 					if (Math.random() < 0.5 && pixel.link1 === null && adjpixel.link1 === null) {
 						pixel.link1 = adjpixel;
 						adjpixel.link1 = pixel;
@@ -34,7 +34,7 @@ elements.paperclip = {
 		let prevPosY = pixel.y;
 		behaviors.POWDER(pixel);
 		let adjpixel = pixel.link1;
-		if (!(adjpixel !== null && adjpixel.element === "paperclip" && currentPixels.includes(adjpixel))) {
+		if (!(adjpixel !== null && (adjpixel.element === "paperclip" || adjpixel.element === "paper") && currentPixels.includes(adjpixel))) {
 			pixel.link1 = null;
 			if (adjpixel !== null && !currentPixels.includes(adjpixel)) {
 				delete pixelMap[adjpixel.x][adjpixel.y];
@@ -42,7 +42,7 @@ elements.paperclip = {
 		}
 		
 		adjpixel = pixel.link2;
-		if (!(adjpixel !== null && adjpixel.element === "paperclip" && currentPixels.includes(adjpixel))) {
+		if (!(adjpixel !== null && (adjpixel.element === "paperclip" || adjpixel.element === "paper") && currentPixels.includes(adjpixel))) {
 			pixel.link2 = null;
 			if (adjpixel !== null && !currentPixels.includes(adjpixel)) {
 				delete pixelMap[adjpixel.x][adjpixel.y];
@@ -53,7 +53,7 @@ elements.paperclip = {
 		{
 			if (Math.random() < 0.5) {
 				adjpixel = pixel.link1;
-				if (adjpixel !== null && adjpixel.element === "paperclip" && currentPixels.includes(adjpixel)) {
+				if (adjpixel !== null && (adjpixel.element === "paperclip" || adjpixel.element === "paper") && currentPixels.includes(adjpixel)) {
 					if(!recursiveMove(adjpixel, prevPosX, prevPosY,true,0))
 					{
 						pixel.link1 = null;
@@ -64,7 +64,7 @@ elements.paperclip = {
 				}
 			} else {
 				adjpixel = pixel.link2;
-				if (adjpixel !== null && adjpixel.element === "paperclip" && currentPixels.includes(adjpixel)) {
+				if (adjpixel !== null && (adjpixel.element === "paperclip" || adjpixel.element === "paper") && currentPixels.includes(adjpixel)) {
 					if(!recursiveMove(adjpixel, prevPosX, prevPosY,false,0))
 					{
 						pixel.link2 = null;
@@ -87,7 +87,7 @@ function recursiveMove(pixel,x,y,left,iters) {
 	{	
 		if(!left) {
 			let adjpixel = pixel.link1;
-			if (adjpixel !== null && adjpixel.element === "paperclip" && currentPixels.includes(adjpixel)) {
+			if (adjpixel !== null && (adjpixel.element === "paperclip" || adjpixel.element === "paper") && currentPixels.includes(adjpixel)) {
 				if(!recursiveMove(adjpixel, prevPosX, prevPosY,!left,iters+1))
 				{
 					pixel.link1 = null;
@@ -99,7 +99,7 @@ function recursiveMove(pixel,x,y,left,iters) {
 			}
 		} else {
 			let adjpixel = pixel.link2;
-			if (adjpixel !== null && adjpixel.element === "paperclip" && currentPixels.includes(adjpixel)) {
+			if (adjpixel !== null && (adjpixel.element === "paperclip" || adjpixel.element === "paper") && currentPixels.includes(adjpixel)) {
 				if(!recursiveMove(adjpixel, prevPosX, prevPosY,!left,iters+1))
 				{
 					pixel.link2 = null;
@@ -111,4 +111,90 @@ function recursiveMove(pixel,x,y,left,iters) {
 		} 
 	}
 	return true;
+}
+
+elements.paper = {
+	color: "#f7f7f7",
+	tempHigh: 400,
+	stateHigh: ["ember","charcoal","fire","fire","fire"],
+	category: "solids",
+	burn: 5,
+	burnTime: 300,
+	burnInto: ["ember","charcoal","fire"],
+	state: "solid",
+	hardness: 0.15,
+	//breakInto: "chad",
+	properties: {
+		link1: null,
+		link2: null,
+		linked: false
+	},
+	tick: function(pixel) {
+		
+		if(!pixel.linked) {
+			for(let i = -1; i <= 1; i++) {
+				let dx = i;
+				let dy = 0;
+				if(!(dx === 0 || dy === 0) && !isEmpty(pixel.x+dx, pixel.y+dy, true))
+				{
+					let adjpixel = pixelMap[pixel.x+dx][pixel.y+dy];
+					if (adjpixel.element === "paper") {
+						if (pixel.link1 === null && adjpixel.link1 === null) {
+							pixel.link1 = adjpixel;
+							adjpixel.link1 = pixel;
+						} else if (pixel.link2 === null && adjpixel.link2 === null) {
+							pixel.link2 = adjpixel;
+							adjpixel.link2 = pixel;
+						}
+					}
+				}
+			}
+			pixel.linked = true;
+		}
+		let prevPosX = pixel.x;
+		let prevPosY = pixel.y;
+		behaviors.POWDER(pixel);
+		let adjpixel = pixel.link1;
+		if (!(adjpixel !== null && (adjpixel.element === "paperclip" || adjpixel.element === "paper") && currentPixels.includes(adjpixel))) {
+			pixel.link1 = null;
+			if (adjpixel !== null && !currentPixels.includes(adjpixel)) {
+				delete pixelMap[adjpixel.x][adjpixel.y];
+			}
+		}
+		
+		adjpixel = pixel.link2;
+		if (!(adjpixel !== null && (adjpixel.element === "paperclip" || adjpixel.element === "paper") && currentPixels.includes(adjpixel))) {
+			pixel.link2 = null;
+			if (adjpixel !== null && !currentPixels.includes(adjpixel)) {
+				delete pixelMap[adjpixel.x][adjpixel.y];
+			}
+		}
+		
+		if(!(prevPosX === pixel.x && prevPosY === pixel.Y))
+		{
+			if (Math.random() < 0.5) {
+				adjpixel = pixel.link1;
+				if (adjpixel !== null && (adjpixel.element === "paperclip" || adjpixel.element === "paper") && currentPixels.includes(adjpixel)) {
+					if(!recursiveMove(adjpixel, prevPosX, prevPosY,true,0))
+					{
+						pixel.link1 = null;
+						adjpixel.link1 = null;
+					}
+				} else {
+					pixel.link1 = null;
+				}
+			} else {
+				adjpixel = pixel.link2;
+				if (adjpixel !== null && (adjpixel.element === "paperclip" || adjpixel.element === "paper") && currentPixels.includes(adjpixel)) {
+					if(!recursiveMove(adjpixel, prevPosX, prevPosY,false,0))
+					{
+						pixel.link2 = null;
+						adjpixel.link2 = null;
+					}
+				} else {
+					pixel.link2 = null;
+				}
+			}
+		}			
+	}
 }
